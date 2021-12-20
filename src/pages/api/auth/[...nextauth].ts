e0a78c7bd7c7557eb0b07b1a83690aa716743e79
Caio Fábio Valentim <72234988+caiogomesxx@users.noreply.github.com>
2021-12-20 11:54:55 -0300

    stripe

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,4 +1,4 @@
-import {query as q } from 'faunadb';
+import {query as q } from 'faunadb'
 
 import NextAuth from "next-auth"
 import GithubProvider from "next-auth/providers/github"
@@ -23,12 +23,28 @@ export default NextAuth({
 
       try {
         await fauna.query(
-          q.Create(
-            q.Collection('users'),
-            {data: { email }}
+          q.If(
+            q.Not(
+              q.Exists(
+                q.Match(
+                  q.Index('user_by_email'),
+                  q.Casefold(user.email)
+                )
+              )
+            ),
+            q.Create(
+              q.Collection('users'),
+              {data: { email }}
+            ),
+            q.Get(
+              q.Match(
+                q.Index('user_by_email'),
+                q.Casefold(user.email)
+            )
           )
         )
-        return true
+        )
+       return true
       } catch {
         return false
       }
